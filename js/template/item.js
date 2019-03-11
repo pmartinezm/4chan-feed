d6830e742f8bd44f035de24b4b1f00f139637c1c
Pablo <peybol9858@gmail.com>
2019-03-12 00:31:16 +0100

    Initial
    Basic view, almost the same as 4chan

---
+++ js/template/item.js
@@ -0,0 +1,13 @@
+var itemTMPL = `<div class="item">
+  <div class="item-image-cont">
+    <a href="$HREF%""><img class="item-image" src="$URL%" alt=""></a>
+  </div>
+  <div class="item-data-cont">
+    <div class="item-data-sub">
+      $SUB%
+    </div>
+    <div class="item-data-com">
+      $COM%
+    </div>
+  </div>
+</div>`;
